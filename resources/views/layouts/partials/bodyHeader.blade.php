5ab83e775d90dcd5581b2ad4bcc3b57fb8fa9f5c
Sam Hoang <samhv.ict@gmail.com>
2016-10-16 04:51:48 +0700

    Show username in navbar if login successful

--- resources/views/layouts/partials/bodyHeader.blade.php
+++ resources/views/layouts/partials/bodyHeader.blade.php
@@ -18,9 +18,14 @@
 
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav">
-                            <li><a href="index.html">Home</a></li>
-                            <li><a href="products.html">Bikini</a></li>
+                            <li><a href="{{ url('/') }}">Home</a></li>
+                            <li><a href="{{ url('/') }}">Bikini</a></li>
                         </ul>
+                        @if(Auth::check())
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="{{ url('/') }}">{{Auth::user()->name}}</a></li>
+                            </ul>
+                        @endif
                     </div>
                     <!--/.navbar-collapse-->
                 </nav>
