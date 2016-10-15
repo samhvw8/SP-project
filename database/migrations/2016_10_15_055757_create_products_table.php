26b671052c033b6db129a63bf221778c89552164
Sam Hoang <samhv.ict@gmail.com>
2016-10-15 13:23:17 +0700

    Init Commit

---
+++ database/migrations/2016_10_15_055757_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->decimal('price', 8, 2);
+            $table->integer('qty')->unsigned();
+            $table->string('url')->nullable();
+            $table->string('info')->nullable();
+            $table->string('sex')->nullable();
+            $table->string('size')->nullable();
+            $table->string('key_word');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
